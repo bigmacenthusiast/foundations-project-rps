cbc296dd5e961c71aef96b1ea0e427c2ce7a4b06
pogtrump <nhtruong95@gmail.com>
2023-08-24 01:08:30 +0700

    refactored and added ui
    reworked functions logic and added an ui with buttons so the game can now be played in browser.

--- script.js
+++ script.js
@@ -1,78 +1,68 @@
-function getComputerChoice(choice) {
-    let numberOfChoice = 3;
-    let randomCase = Math.floor(Math.random() * numberOfChoice) + 1;
+function getComputerChoice() {
+    let choiceList = ['rock', 'paper', 'scissors'];
+    let randomChoice = Math.floor(Math.random() * choiceList.length);
+    return choiceList[randomChoice];
+}
 
-    if (randomCase === 1) {
-        choice = "rock";
-        return choice;
-    }
-    else if (randomCase === 2) {
-        choice = "paper";
-        return choice;
-    }
-    else {
-        choice = "scissors";
-        return choice;
+function playRound(playerChoice, computerChoice) {
+    
+    if (playerChoice === computerChoice) {
+        return `Tie`;
+    } else if (playerChoice === 'rock' && computerChoice === 'paper' || 
+               playerChoice === 'paper' && computerChoice === 'scissors' ||
+               playerChoice === 'scissors' && computerChoice === 'rock') {
+        computerScore++;
+        return `You Lose! ${computerChoice} beats ${playerChoice}`;
+    } else {
+        playerScore++;
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
     }
 }
+
+
 
 let playerScore = 0;
 let computerScore = 0;
-let rounds = 5;
-let currentRound = 1;
 
-function playRound(playerChoice, computerChoice) {
-  console.log(`Welcome to round ${currentRound}!`);  
-  playerChoice = prompt(`Round ${currentRound}. Choose Rock, Paper, or Scissors?`).toLowerCase();
-  console.log(`Player choses ${playerChoice}.`);
-  computerChoice = getComputerChoice();
-  console.log(`Computer choses ${computerChoice}.`);
+const buttons = document.querySelectorAll('.button');
 
-  if (playerChoice === computerChoice) {
-    console.log(`It\'s a tie!`);
-    console.log(`Score is now Player ${playerScore} - ${computerScore} Computer.`);
-    console.log(`Round ${currentRound} ties.`);
-    ++currentRound;
-    return `Round ${currentRound} ties.`;
-  } else if (playerChoice === "rock" && computerChoice === "scissors" ||
-                playerChoice === "paper" && computerChoice === "rock" ||
-                playerChoice === "scissors" && computerChoice === "paper") {
-    console.log(`Player wins. ${playerChoice} beats ${computerChoice}.`);
-    playerScore++;
-    console.log(`Score is now Player ${playerScore} - ${computerScore} Computer.`);
-    console.log(`Round ${currentRound}: Player wins.`);
-    ++currentRound;
-    return `Round ${currentRound}: Player wins.`;
-  } else {
-    console.log(`Player loses. ${computerChoice} beats ${playerChoice}.`);
-    computerScore++;
-    console.log(`Score is now Player ${playerScore} - ${computerScore} Computer.`);
-    console.log(`Round ${currentRound}: Player loses.`);
-    ++currentRound;
-    return `Round ${currentRound}: Player loses.`;
-  } 
-}
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const playerChoice = button.getAttribute('id');   
+        const computerChoice = getComputerChoice();                                                                 
+        const result = playRound(playerChoice,computerChoice);
+        
+        const resultDiv = document.createElement('div');
+        resultDiv.textContent = `${result}`;
+        document.body.appendChild(resultDiv);
 
-function determineWinner() {
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You win!");
-    } else if (playerScore < computerScore) {
-        console.log("Computer wins! Better luck next time!");
-    } else {
-        console.log("It\'s a tie! Well played both sides!")
+        const scoreDiv = document.createElement('div');
+        scoreDiv.textContent = `${playerScore} - ${computerScore}`;
+        document.body.appendChild(scoreDiv);
+
+        declareWinner();
+        
+    });
+});
+function declareWinner() {
+    const winnerAnnouncementElement = document.createElement('div');
+    winnerAnnouncementElement.textContent
+    document.body.appendChild(winnerAnnouncementElement);
+    if(playerScore === 5) {
+        winnerAnnouncementElement.textContent = 'Player wins.';
+    } else if(computerScore === 5) {
+        winnerAnnouncementElement.textContent = 'Computer wins.';
     }
 }
 
-function game() {
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    console.log(`Final score - Player: ${playerScore}, Computer: ${computerScore}`);
-    determineWinner();
-}
-game();
+
+
+
+
+
+
+
+
 
 
 
